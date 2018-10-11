6d8bc1fd05517140ccb664887958dbdf17848602
Anton <metallist9656@gmail.com>
2018-10-11 23:13:09 +0300

    Add files via upload
    testing demo app

---
+++ work/build/precache-manifest.135e2aa10919c2cb153cc126cae051ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "51f85ff4292934d9a2584cb4c7b219b4",
+    "url": "/static/media/car2.51f85ff4.png"
+  },
+  {
+    "revision": "88418aff650793a58e968010aff908d3",
+    "url": "/static/media/404-min.88418aff.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0049b7444e3f9f22e25c",
+    "url": "/static/js/main.0049b744.chunk.js"
+  },
+  {
+    "revision": "5bc69657ea471b8794c3",
+    "url": "/static/js/1.5bc69657.chunk.js"
+  },
+  {
+    "revision": "0049b7444e3f9f22e25c",
+    "url": "/static/css/main.216931a5.chunk.css"
+  },
+  {
+    "revision": "5bc69657ea471b8794c3",
+    "url": "/static/css/1.c548ea08.chunk.css"
+  },
+  {
+    "revision": "042674afeef85fb5d33b89b2c2fda06e",
+    "url": "/index.html"
+  }
+];
